a035492506f0b1357cec060a34802cbbcca332c1
Steven <sacastillo889@misena.edu.co>
2025-01-23 14:19:03 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,35 @@
+import math
+
+
+print('\nProyecto Maquina de Goldberg \n')
+
+altura=0
+peso=0
+gravedad=9.8
+
+def tiempo_caida_libre(altura, caida):
+    tiempo=(math.sqrt(((altura/100)*2)/gravedad))
+    print(f'Tiempo caida {caida}: {round(tiempo,3)}s - Altura: {altura}cm')
+    
+    
+##
+
+def mur(altura, tramo):
+    tiempo=(math.sqrt(((altura/100)*2)/gravedad))
+    velocidad_final=0+gravedad*tiempo
+    print(f'caida {tramo}   |   {altura}cm         |   {round(tiempo,3)}s    |   {gravedad}m/s^2         |   {round(velocidad_final,3)}cm/s')
+        
+    
+##
+
+tiempo_caida_libre(3,1)
+tiempo_caida_libre(3,2)
+tiempo_caida_libre(3,3)
+tiempo_caida_libre(7,4)
+
+print(f'Nombre    |   Distancia   |   Tiempo    |   Aceleración      |   Velocidad final')
+mur(3,1)
+mur(3,2)
+mur(3,3)
+mur(7,4)
+
